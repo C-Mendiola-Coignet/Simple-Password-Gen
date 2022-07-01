0581cf1013416419bd0fcd822bf53d8c120edec2
Leticia Mendiola <CMendiola2022@gmail.com>
2022-07-01 17:56:40 -0500

    added prompt to determine password length

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// prompt for character length 
+var lengthPrompt = function () {
+  charLength = prompt("Select password character length (min 8 - max 128)", "Type number here");
+  charLength = Math.floor(charLength);
+  if (isNaN(charLength)) {
+    alert("Input must be a whole number");
+    return;
+  } else if (charLength < 8 || charLength > 128) {
+    alert("Number must be between 8 - 128");
+    return;
+  } else {
+    typeConfirm();
+  }
+}
 
 // Write password to the #password input
 function writePassword() {
